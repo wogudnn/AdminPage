1f7d798bf0b728b59339918f0637f53b9fc0613d
206-024\206-024 <n2350@naver.com>
2016-11-29 11:09:17 +0900

    no message

--- src/main/java/com/ktdsuniv/lecture/web/LectureController.java
+++ src/main/java/com/ktdsuniv/lecture/web/LectureController.java
@@ -1,6 +1,8 @@
 package com.ktdsuniv.lecture.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.ktdsuniv.lecture.service.LectureService;
 
@@ -11,6 +13,14 @@ public class LectureController {
 	
 	public void setLectureService(LectureService lectureService) {
 		this.lectureService = lectureService;
+	}
+	
+	@RequestMapping("/lectureList")
+	public ModelAndView viewLectureListPage() {
+		
+		ModelAndView view = new ModelAndView();
+		view.setViewName("lecture/lecture");
+		return view;
 	}
 	
 }
